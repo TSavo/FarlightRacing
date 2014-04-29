50423e554729cc6b84d5908d6baec362afbe3a09
Travis Savo <evilgenius@nefariousplan.com>
2014-04-29 02:18:33 -0700

    Making race registration go-like

--- server.go
+++ server.go
@@ -7,9 +7,7 @@ import (
 	//"fmt"
 	"bufio"
 	"strconv"
-	"sync"
 )
-
 
 type RaceLobby struct {
 	*Race
@@ -18,14 +16,15 @@ type RaceLobby struct {
 }
 
 type Server struct {
-	Races   []*Race
-	Lobbies []*RaceLobby
-	Lock    *sync.Mutex
+	Races        []*Race
+	Lobbies      []*RaceLobby
+	RegisterChan chan *JoinMessage
 }
 
 type JoinMessage struct {
 	Name, Track, Prototype, Password string
 	NumPlayers                       int
+	Conn                             net.Conn
 }
 
 func GetPrototype(name string) *Prototype {
@@ -52,48 +51,53 @@ func GetTrack(name string) *Track {
 	return &Track{"testtrack", "Test Track", walls, goal}
 }
 
+func (this *Server) RegisterThread() {
+	for {
+		joinMessage := <-this.RegisterChan
+		player := Player{Name: joinMessage.Name, Conn: joinMessage.Conn}
+		var lobby *RaceLobby = nil
+		for _, race := range this.Lobbies {
+			if race.Race.Track.Id == joinMessage.Track && race.NumPlayers == joinMessage.NumPlayers && race.Password == joinMessage.Password {
+				lobby = race
+				break
+			}
+		}
+		if(lobby == nil) {
+			lobby = &RaceLobby{NewRace(GetTrack(joinMessage.Track)), joinMessage.NumPlayers, joinMessage.Password}
+			this.Lobbies = append(this.Lobbies, lobby)
+		}
+		lobby.Race.RegisterRacer(&player, GetPrototype(joinMessage.Prototype))
+		trackMessage := make(map[string]interface{})
+		trackMessage["Track"] = lobby.Race.Track
+		trackMessage["Race"] = lobby.Race.Id
+		player.Send("RaceData", trackMessage)
+		this.StartRaces();
+	}
+}
+
+func (this *Server) StartRaces(){
+	for x := 0; x < len(this.Lobbies); x++ {
+		race := this.Lobbies[x]
+		if race.NumPlayers == len(race.Race.Ships) {
+			this.Races = append(this.Races, race.Race)
+			this.Lobbies = append(this.Lobbies[:x], this.Lobbies[x+1:]...)
+			x--
+			go race.Race.RunRace()
+		}
+	}
+}
+
 func (this *Server) HandleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, _, _ := reader.ReadLine()
 	joinMessage := JoinMessage{}
 	json.Unmarshal(message, &joinMessage)
-
-	player := Player{Name: joinMessage.Name, Conn: conn}
-	this.Lock.Lock()
-	removeRace := -1
-	foundRace := false
-	for x, race := range this.Lobbies {
-		if race.Race.Track.Id == joinMessage.Track && race.NumPlayers == joinMessage.NumPlayers && race.Password == joinMessage.Password {
-			race.Race.RegisterRacer(&player, GetPrototype(joinMessage.Prototype))
-			foundRace = true
-			if race.NumPlayers == len(race.Race.Ships) {
-				this.Races = append(this.Races, race.Race)
-				go race.Race.RunRace()
-				removeRace = x
-			}
-			break
-		}
-	}
-	if removeRace > -1 {
-		this.Lobbies = append(this.Lobbies[:removeRace], this.Lobbies[removeRace+1:]...)
-	}
-	if foundRace {
-		this.Lock.Unlock()
-		return
-	}
-	track := GetTrack(joinMessage.Track)
-	lobby := RaceLobby{NewRace(track), joinMessage.NumPlayers, joinMessage.Password}
-	lobby.Race.RegisterRacer(&player, GetPrototype(joinMessage.Prototype))
-	this.Lobbies = append(this.Lobbies, &lobby)
-	this.Lock.Unlock()
-	trackMessage := make(map[string]interface{})
-	trackMessage["Track"] = track
-	trackMessage["Race"] = lobby.Race.Id
-	writer := json.NewEncoder(conn)
-	writer.Encode(message)
+	joinMessage.Conn = conn
+	this.RegisterChan <- &joinMessage
 }
 
 func (this *Server) Listen(port int) {
+	go this.RegisterThread()
 	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		return
